02e4aef302df66f57b1ee39c320f4cfdb01e5792
sebamed <seba.med@yahoo.com>
2019-03-26 22:49:51 +0100

    Added undo/redo implementation

--- src/main/controller/PaintController.java
+++ src/main/controller/PaintController.java
@@ -2,8 +2,11 @@ package main.controller;
 
 import java.awt.Color;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 import main.model.ShapesModel;
+import main.model.command.Command;
 import main.model.command.add.AddShapeCmd;
 import main.model.shape.Point;
 import main.view.Paint;
@@ -13,18 +16,50 @@ public class PaintController {
 	private ShapesModel model;
 	private Paint paint;
 
+	private List<Command> commands = new ArrayList<>();
+	private int currentCommandIndex = -1;
+
 	public PaintController(ShapesModel model, Paint paint) {
 		this.model = model;
 		this.paint = paint;
 	}
-	
+
 	public void handleMouseClick(MouseEvent e) {
 		Point point = new Point(e.getX(), e.getY(), Color.BLACK);
-		this.model.getShapes().add(point);
 		AddShapeCmd addShape = new AddShapeCmd(point, this.model);
-		addShape.execute();
-		
+		helpCommandExecution(addShape);
 		this.paint.getCanvas().repaint();
+	}
+
+	private void helpCommandExecution(Command command) {
+		command.execute();
+		this.commands.add(command);
+		this.currentCommandIndex++;
+		this.setUndoRedoNavigation();
+	}
+
+	public void undo() {
+		this.commands.get(this.currentCommandIndex).unexecute();
+		this.currentCommandIndex--;
+		this.setUndoRedoNavigation();
+		this.repaint();
+	}
+
+	public void redo() {
+		this.currentCommandIndex++;
+		this.commands.get(currentCommandIndex).execute();
+		this.setUndoRedoNavigation();
+		this.repaint();
+	}
+
+	private void setUndoRedoNavigation() {
+		this.paint.setEnabledBtnUndo(this.currentCommandIndex > -1);
+		this.paint.setEnabledBtnRedo(this.currentCommandIndex < this.commands.size() - 1);
+		this.repaint();
+	}
+
+	private void repaint() {
+		this.paint.repaint();
 	}
 
 }
--- src/main/view/Paint.java
+++ src/main/view/Paint.java
@@ -1,6 +1,7 @@
 package main.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -8,12 +9,21 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import main.controller.PaintController;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Paint extends JFrame {
 
 	private PaintController controller;
 
 	private Canvas canvas = new Canvas();
+	private JPanel topCommandsPanel = new JPanel();
+	
+	/**
+	 * Undo Redo Navigation
+	 */
+	private final JButton btnRedo = new JButton("Redo");
+	private final JButton btnUndo = new JButton("Undo");
 
 	public Paint(int width, int height) {
 		this.setSize(520, 409);
@@ -24,6 +34,10 @@ public class Paint extends JFrame {
 		JPanel mainPanel = new JPanel();
 		getContentPane().add(mainPanel, BorderLayout.CENTER);
 		mainPanel.setLayout(new BorderLayout(0, 0));
+
+		/**
+		 * canvas mouse event handler
+		 */
 		canvas.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -31,6 +45,33 @@ public class Paint extends JFrame {
 			}
 		});
 
+		this.topCommandsPanel.setBackground(Color.black);
+
+		mainPanel.add(this.topCommandsPanel, BorderLayout.NORTH);
+
+		/**
+		 * undo button event handler
+		 */
+		btnUndo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				controller.undo();
+			}
+		});
+		
+		btnRedo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				controller.redo();
+			}
+		});
+
+		topCommandsPanel.add(btnUndo);
+		topCommandsPanel.add(btnRedo);
+		
+		this.setEnabledBtnUndo(false);
+		this.setEnabledBtnRedo(false);
+		
 		mainPanel.add(this.canvas, BorderLayout.CENTER);
 
 	}
@@ -50,4 +91,13 @@ public class Paint extends JFrame {
 	public void setCanvas(Canvas canvas) {
 		this.canvas = canvas;
 	}
+	
+	public void setEnabledBtnUndo(boolean enabled) {
+		this.btnUndo.setEnabled(enabled);
+	}
+	
+	public void setEnabledBtnRedo(boolean enabled)  {
+		this.btnRedo.setEnabled(enabled);
+	}
+	
 }
