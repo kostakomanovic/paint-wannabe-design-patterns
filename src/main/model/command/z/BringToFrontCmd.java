b0f97edc8351df6530d0b1f3e3a4cb213d13d794
sebamed <seba.med@yahoo.com>
2019-03-29 11:17:02 +0100

    Added Z-Axis commands functionalities

---
+++ src/main/model/command/z/BringToFrontCmd.java
@@ -0,0 +1,38 @@
+package main.model.command.z;
+
+import main.model.ShapesModel;
+import main.model.command.Command;
+import main.model.shape.base.Shape;
+
+public class BringToFrontCmd implements Command {
+	
+	private Shape shape;
+	private ShapesModel model;
+	private int currentIndex = -1;
+	
+	
+
+	public BringToFrontCmd(ShapesModel model, int currentIndex) {
+		this.model = model;
+		this.currentIndex = currentIndex;
+		this.shape = this.model.getShapes().get(this.currentIndex);
+	}
+
+	@Override
+	public void execute() {
+		this.model.getShapes().remove(this.shape);
+		this.model.getShapes().add(this.shape);
+	}
+
+	@Override
+	public void unexecute() {
+		this.model.getShapes().remove(this.shape);
+		this.model.getShapes().add(0, this.shape);
+	}
+
+	@Override
+	public String toString() {
+		// command,shape
+		return "bringtofront," + this.shape;
+	}
+}
---
+++ src/main/model/command/z/ToFrontCmd.java
@@ -0,0 +1,42 @@
+package main.model.command.z;
+
+import java.util.Collections;
+
+import main.model.ShapesModel;
+import main.model.command.Command;
+import main.model.shape.base.Shape;
+
+public class ToFrontCmd implements Command {
+
+	private Shape shape;
+	private ShapesModel model;
+
+	public ToFrontCmd(Shape shape, ShapesModel model) {
+		this.shape = shape;
+		this.model = model;
+	}
+
+	@Override
+	public void execute() {
+		int shapeIndex = this.model.getShapes().indexOf(this.shape);
+		if (shapeIndex != -1 && shapeIndex != (this.model.getShapes().size() - 1)) {
+			Collections.swap(this.model.getShapes(), shapeIndex, shapeIndex + 1);
+		}
+	}
+
+	@Override
+	public void unexecute() {
+		int shapeIndex = this.model.getShapes().indexOf(this.shape);
+		if (shapeIndex > 0) {
+			Collections.swap(this.model.getShapes(), shapeIndex, shapeIndex - 1);
+		}
+		;
+	}
+
+	@Override
+	public String toString() {
+		// command,shape
+		return "toftont," + this.shape;
+	}
+
+}
